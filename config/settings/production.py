6982ea67e0b7166a358462cf8caac13991c5ae7b
kimdonghui <kpho97@gmail.com>
2018-09-13 17:10:55 +0900

    final

--- config/settings/production.py
+++ config/settings/production.py
@@ -20,9 +20,9 @@ DATABASES = {
     }
 }
 # ------------------------------------------------------------------------------
-DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
-DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
-DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
+#DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
+#DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
+#DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
 
 # CACHES
 # ------------------------------------------------------------------------------
